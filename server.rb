4ba6e53568c2b77b73b816eadc438b2603bbc81c
Darian Jarvis <djarvis@nyc.yearup.org>
2020-01-07 11:48:15 -0500

    sign up problem has been fixed.

--- server.rb
+++ server.rb
@@ -27,55 +27,55 @@ post '/' do #CREATE new user to go be INSERTed to database
 
 end
 #=======================================
-# get '/login' do
-#
-#   erb :login
-# end
-#
-# post '/login' do
-#
-#   @user = User.find_by(email: params[:user]['email'])
-#
-#   @given_password = params[:user]['password']
-#
-#   if @user.password == @given_password
-#     session[:user_id] = @user.id
-#
-#     redirect "/profile/#{@user.id}"
-#   end
-# end
-#
-# get '/logout' do
-#   session.clear
-#   redirect '/'
-# end
+get '/login' do
+
+  erb :login
+end
+
+post '/login' do
+
+  @user = User.find_by(email: params[:user]['email'])
+
+  @given_password = params[:user]['password']
+
+  if @user.password == @given_password
+    session[:user_id] = @user.id
+
+    redirect "/profile/#{@user.id}"
+  end
+end
+
+get '/logout' do
+  session.clear
+  redirect '/'
+end
 
 #=======================================
-# get "/profile" do
-#
-#   redirect "/profile/#{@user.id}"
-#
-# end
+get "/profile" do
 
-#
-# get "/profile/:id" do
-#   @user = User.find_by(id: params['id'])
-#   pp @user
-#
-#   redirect '/' unless
-#   session[:user_id]
-#
-#   erb :nav, :layout => :profile
-#
-#   rescue ActiveRecord::RecordNotFound
-#     puts 'ERROR 404'
-#     erb :start
-#
-#
-#
-# end
-#
-#
+  redirect "/profile/#{@user.id}"
+
+end
+
+
+get "/profile/:id" do
+  @user = User.find_by(id: params['id'])
+  pp @user
+
+  redirect '/' unless
+  session[:user_id]
+
+  erb :nav, :layout => :profile
+
+  rescue ActiveRecord::RecordNotFound
+    puts 'ERROR 404'
+    erb :start
+
+
+
+end
+
+
 # get '/feed' do
 #   # get user ID from session
 #   if session['user_id']
